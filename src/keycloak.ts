e1ea8e231be23230e01ead07415c965effd99a0f
keha <keha.chandrakar@9envoy.com>
2026-01-28 19:10:50 +0530

    added keycloak

---
+++ src/keycloak.ts
@@ -0,0 +1,15 @@
+import Keycloak from "keycloak-js";
+
+console.log("🔐 Keycloak config:", {
+  url: import.meta.env.VITE_KEYCLOAK_URL,
+  realm: import.meta.env.VITE_KEYCLOAK_REALM,
+  clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
+});
+
+const keycloak = new Keycloak({
+  url: import.meta.env.VITE_KEYCLOAK_URL,
+  realm: import.meta.env.VITE_KEYCLOAK_REALM,
+  clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
+});
+
+export default keycloak;
